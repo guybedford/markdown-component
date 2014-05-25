e6193a773603fd0f348b480b80fce533c472662d
guybedford <guybedford@gmail.com>
2014-05-25 13:55:50 -0700

    release

--- config.js
+++ config.js
@@ -8,17 +8,17 @@ System.config({
 
 System.config({
   "map": {
-    "css": "github:systemjs/plugin-css@master",
     "showdown": "github:coreyti/showdown@^0.3.1",
-    "prism": "github:LeaVerou/prism@gh-pages"
+    "prism": "github:LeaVerou/prism@gh-pages",
+    "css": "github:systemjs/plugin-css@master"
   }
 });
 
 System.config({
   "versions": {
-    "github:systemjs/plugin-css": "master",
     "github:coreyti/showdown": "0.3.1",
-    "github:LeaVerou/prism": "gh-pages"
+    "github:LeaVerou/prism": "gh-pages",
+    "github:systemjs/plugin-css": "master"
   }
 });
 
--- markdown-component.js
+++ markdown-component.js
@@ -5,12 +5,13 @@ import 'prism/themes/prism-okaidia.css!';
 
 var converter = new showdown.converter();
 
-export class XMarkdownComponent extends HTMLElement {
+class XMarkdownComponent extends HTMLElement {
   createdCallback() {
     this.innerHTML = converter.makeHtml(
       this.innerHTML.split('\n').map((line) => line.trim()).join('\n')
     );
-    prism.highlightElement(this.querySelector('code'));
+    prism.highlightAll(this.querySelectorAll('code'));
   }
 }
+
 document.registerElement('x-markdown-component', XMarkdownComponent);
